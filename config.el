eedf8c8a6c6fe0778768abf4f2451f1198ad720a
bettafish15 <bettafish6@gmail.com>
2024-05-04 09:49:16 +0700

    add tailwind plugin lsp

--- config.el
+++ config.el
@@ -180,6 +180,9 @@
 
 
 (use-package! lsp-tailwindcss)
+(use-package lsp-tailwindcss
+  :init
+  (setq lsp-tailwindcss-add-on-mode t))
 (use-package! tree-sitter
   :config
   (require 'tree-sitter-langs)
